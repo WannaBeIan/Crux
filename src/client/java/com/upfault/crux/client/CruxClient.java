cc90b583362716f195faedad5d9ccedf1b143638
WannaBeIan <johian2004@gmail.com>
2025-01-06 16:46:36 -0600

    Initial Commit

---
+++ src/client/java/com/upfault/crux/client/CruxClient.java
@@ -0,0 +1,10 @@
+package com.upfault.crux.client;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class CruxClient implements ClientModInitializer {
+
+	@Override
+	public void onInitializeClient() {
+	}
+}
---
+++ src/main/java/com/upfault/crux/Crux.java
@@ -0,0 +1,10 @@
+package com.upfault.crux;
+
+import net.fabricmc.api.ModInitializer;
+
+public class Crux implements ModInitializer {
+
+	@Override
+	public void onInitialize() {
+	}
+}
